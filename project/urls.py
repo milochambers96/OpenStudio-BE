6390f984d37363039a653b0dd22b64476f657578
Milo Chambers <chambersmilo@gmail.com>
2024-12-05 11:49:51 +0000

    feat: add rout route to urls.py to API endpoint documentation

--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
 
+
+def root_route(request):
+    return JsonResponse({
+        "message": "Welcome to OpenStudio API",
+        "endpoints": {
+            "members": "/api/members/",
+            "artworks": "/api/artworks/",
+            "images": "/api/images/",
+            "galleries": "/api/galleries/",
+            "orders": "/api/orders/"
+        }
+    })
+
 urlpatterns = [
+    path('', root_route), 
     path('api/admin/', admin.site.urls),
     path('api/members/', include('members.urls')),
     path('api/artworks/', include('artworks.urls')),
